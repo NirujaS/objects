272dc152e9939bab1233dbda55f58987716a0744
Dell <40-bis-0022@kdu.ac.lk>
2023-10-04 16:42:21 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+        public static void main(String[] args) {
+            // Create two Student objects with names "Sam" and "John"
+            Student sam = new Student("Sam");
+            Student john = new Student("John");
+
+            // Access and print the names of the students
+            System.out.println("Student 1: " + sam.getName());
+            System.out.println("Student 2: " + john.getName());
+        }
+    }
+
---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+        private String name;
+
+        // Constructor to initialize the student's name
+        public Student(String name) {
+            this.name = name;
+        }
+
+        // Getter method to retrieve the student's name
+        public String getName() {
+            return name;
+        }
+    }
+
+
+
+
